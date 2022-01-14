929efa2abb915684e14c14cb88295c53ed521516
apgallego <apgallego@users.noreply.github.com>
2022-01-14 12:55:02 +0100

    Update ej1.js

--- ej1/js/ej1.js
+++ ej1/js/ej1.js
@@ -3,13 +3,15 @@ var contador = 0;
 var items = document.getElementsByClassName('container');
 
 function showItems(){
-    // console.log(items[contador]);
-    try{
-        items[contador].style.display = 'flex';
-        items[contador].addEventListener('click', clickItem);
-        items[contador].addEventListener('mouseover', function(event){event.target.style.cursor = 'pointer'});
-    } catch (err){/*nada, solo capturamos la excepción para que no se muestre*/}
-    contador++;
+    items[contador].style.display = 'flex';
+    items[contador].addEventListener('click', clickItem);
+    items[contador].addEventListener('mouseover', function(event){event.target.style.cursor = 'pointer'});
+    if(contador < items.length - 1){
+        contador++;
+    } else {
+        contador = 0;
+        clearTimeout(show);
+    }
     show = setTimeout(showItems, 1000);
 }
 
