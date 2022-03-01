52c11a8f055eb0634d60cbd821868d50ac9ee941
apgallego <72606018+apgallego@users.noreply.github.com>
2022-03-01 20:43:57 +0100

    ejercicios hechos en o para clase (no todos)

---
+++ ej3/ej3.js
@@ -0,0 +1,8 @@
+let muted = true;
+let play = document.querySelector('.play');
+let stop = document.querySelector('.stop');
+let moreVol = document.querySelector('.+vol');
+let lessVol = document.querySelector('.-vol');
+let toggleMute = document.querySelector('.toggle-mute');
+
+play.addEventListener('click', );
